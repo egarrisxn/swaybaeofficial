9045f84c3cb550068877c65f4539a0c7adb62f21
EGARRISXN <egarrisxn@gmail.com>
2023-07-04 05:35:32 -0400

    colors

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,42 +1,32 @@
+import SwapIcon from "./SwapIcon";
+
 const Nav = () => {
     return (
-        <div className="navbar bg-base-100">
-        <div className="navbar-start">
+        <div className="navbar">
+        <div className="navbar-start text-primary">
             <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                <li><a>Item 1</a></li>
-                <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                    <li><a>Submenu 1</a></li>
-                    <li><a>Submenu 2</a></li>
-                </ul>
-                </li>
-                <li><a>Item 3</a></li>
+                <li><a>About Me!</a></li>
+                <li><a>Where To Find Me</a></li>
+                <li><a>Merch</a></li>
+                <li><a>Business Inquiries</a></li>
             </ul>
             </div>
-            <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+            <a className="btn btn-ghost normal-case text-xl  bg-gradient-to-r from-primary via-urgent to-secondary bg-clip-text text-transparent">Sway!</a>
         </div>
         <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">
-            <li><a>Item 1</a></li>
-            <li tabIndex={0}>
-                <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                    <li><a>Submenu 1</a></li>
-                    <li><a>Submenu 2</a></li>
-                </ul>
-                </details>
-            </li>
-            <li><a>Item 3</a></li>
+            <ul className="menu menu-horizontal space-x-4 px-1">
+                <li><a className=" bg-gradient-to-r from-primary via-urgent to-secondary bg-clip-text text-transparent">About Me!</a></li>
+                <li><a className=" bg-gradient-to-r from-primary via-urgent to-secondary bg-clip-text text-transparent">Where To Find Me</a></li>
+                <li><a className=" bg-gradient-to-r from-primary via-urgent to-secondary bg-clip-text text-transparent">Merch</a></li>
+                <li><a className=" bg-gradient-to-r from-primary via-urgent to-secondary bg-clip-text text-transparent">Business Inquiries</a></li>
             </ul>
         </div>
-        <div className="navbar-end">
-            <a className="btn">Button</a>
+        <div className="navbar-end text-primary">
+            <SwapIcon />
         </div>
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,32 +12,24 @@ module.exports = {
 
   daisyui: {
     themes: [
-      {
-        mytheme: {
-          'primary': '#f9fafb',
-          'primary-focus': '#d1d5db',
-          'primary-content': '#1f2937',
-          'secondary': '#f9fafb',
-          'secondary-focus': '#d1d5db',
-          'secondary-content': '#1f2937',
-          'accent': '#f9fafb',
-          'accent-focus': '#d1d5db',
-          'accent-content': '#1f2937',
-          'neutral': '#f9fafb',
-          'neutral-focus': '#d1d5db',
-          'neutral-content': '#1f2937',
-          'base-100': '#f9fafb',
-          'base-200': '#d1d5db',
-          'base-300': '#1f2937',
-          'base-content': '#f9fafb',
-          'info': '#f9fafb',
-          'success': '#f9fafb',
-          'warning': '#f9fafb',
-          'error': '#f9fafb',
-        },
-      },
+      // {
+      //   mytheme: {
+      //     'primary': '#00ceff',
+      //     'secondary': '#b567ad',
+      //     'accent': '#1a5462',
+      //     'neutral': '#f9fafb',
+      //     'base-100': '#f9fafb',
+      //     'base-200': '#d1d5db',
+      //     'base-300': '#1f2937',
+      //     'base-content': '#f9fafb',
+      //     'info': '#a5b4fc',
+      //     'success': '#36d399',
+      //     'warning': '#fbbd23',
+      //     'error': '#f87272',
+      //   },
+      // },
+      'light',
       'dark',
-      'light'
     ]
   }
 }
