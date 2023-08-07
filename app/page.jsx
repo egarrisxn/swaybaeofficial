c577799e1c5b7a145c1d79a3fe66ca2675680001
unknown <egarrisxn@gmail.com>
2023-08-07 02:49:09 -0400

    debug

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,20 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import LogoWhite from '@/public/images/logowhite.png';
-import CustomButton from '@/components/CustomButton';
+import Link from "next/link";
+import Image from "next/image";
+import LogoWhite from "@/public/images/logowhite.png";
+import CustomButton from "@/components/CustomButton";
 
 export default function Home() {
   return (
     <>
-      <main className='hero'>
-        <div className='hero-content flex flex-col items-center'>
-          <div className='mb-4'>
-            <Link href='/'>
+      <main className="hero">
+        <div className="hero-content flex flex-col items-center">
+          <div className="mb-4">
+            <Link href="/">
               <Image
                 src={LogoWhite}
                 width={400}
                 height={200}
-                alt='Logo White'
+                alt="Logo White"
                 priority
               />
             </Link>
