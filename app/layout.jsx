064e6ec24f531ac463eb3e72267a5e56b6d9020e
EGARRISXN <egarrisxn@gmail.com>
2024-05-11 15:11:34 -0400

    suppresshydrowarning

--- app/layout.jsx
+++ app/layout.jsx
@@ -58,7 +58,7 @@ export const metadata = {
 
 export default function RootLayout({children}) {
   return (
-    <html lang='en'>
+    <html lang='en' suppressHydrationWarning>
       <body className={`${lobster.variable} ${poppins.variable}`}>
         <ThemeProvider attribute='class' defaultTheme='system'>
           {children}
