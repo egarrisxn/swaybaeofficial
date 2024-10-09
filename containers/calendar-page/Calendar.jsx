511b2fae6ed8f2bc70cdde88ca5e098f19a8922b
EGARRISXN <egarrisxn@gmail.com>
2024-10-09 19:07:30 -0400

    calendar bug

--- containers/calendar-page/Calendar.jsx
+++ containers/calendar-page/Calendar.jsx
@@ -18,7 +18,7 @@ export default function Calendar() {
       <section className='mx-auto mb-16 mt-12 max-w-7xl sm:px-4 lg:my-20 lg:px-6 xl:px-10 3xl:my-24 3xl:px-16 4xl:max-w-screen-3xl'>
         <div className='rounded-lg border-2 border-b2w bg-w2b p-1 shadow-soft hover:shadow-hard md:p-2 xl:p-4'>
           <GoogleCalendar />
-          <div className='mb-0.5 mr-0.5 mt-1.5 flex justify-end md:mt-2 xl:mt-4'>
+          {/* <div className='mb-0.5 mr-0.5 mt-1.5 flex justify-end md:mt-2 xl:mt-4'>
             <Button asChild variant='fun' size='sm'>
               <a
                 href='https://calendar.google.com/calendar/u/0/r?cid=63739436a2ea120a341ec13abffc4b1289f77240a837d12043bed81ce6c78e1c@group.calendar.google.com'
@@ -28,7 +28,7 @@ export default function Calendar() {
                 Add to Your Calendar
               </a>
             </Button>
-          </div>
+          </div> */}
         </div>
       </section>
     </>
