2ea625c5307ea4e75d5a353c70ff2a23da4b5cde
egarrisxn <egarrisxn@gmail.com>
2025-01-10 13:00:23 -0500

    new date for foooter

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,6 +2,7 @@
 import SocialIcons from './social-icons'
 
 export default function Footer() {
+  const date = new Date().getFullYear()
   return (
     <footer className='w-full'>
       <div className='px-4 py-6 md:px-6'>
@@ -11,7 +12,9 @@ export default function Footer() {
           </section>
           <section className='flex items-center justify-center'>
             <p className='bg-gradient-to-bl from-primary-tint via-secondary-tint to-dark bg-clip-text text-base font-semibold tracking-tight text-transparent lg:text-lg 2xl:text-2xl 3xl:text-3xl dark:bg-gradient-to-tl dark:from-primary-fade dark:via-secondary-fade dark:to-primary-tint'>
-              <span className='pr-0.5 font-normal text-foreground'>© 2024 </span>
+              <span className='pr-0.5 font-normal text-foreground'>
+                © {date}{' '}
+              </span>
               Sway Bae Official
             </p>
           </section>
